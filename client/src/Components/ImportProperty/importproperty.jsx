bd43bc2d49e0b511cd617fcc21166a19d88fc6cd
Pawan Piumal <pawanpiumal1@gmail.com>
2024-01-20 23:23:47 +0530

    Test

--- client/src/Components/ImportProperty/importproperty.jsx
+++ client/src/Components/ImportProperty/importproperty.jsx
@@ -16,7 +16,7 @@ class importproperty extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            xml: "<x>Remove this</x>",
+            xml: "<x>Remove this Code</x>",
             isLoading: false,
             jsonResult: { result: "result" }
         }
