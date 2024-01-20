5eac58cfe4180d5a95df216b886fa017be0d75f0
Pawan Piumal <pawanpiumal1@gmail.com>
2024-01-20 21:20:21 +0530

    loader and JSON

--- client/src/Components/ImportProperty/importproperty.jsx
+++ client/src/Components/ImportProperty/importproperty.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 import XMLViewer from 'react-xml-viewer'
+import ReactJson from 'react-json-view'
+
 
 import './importproperty.css'
 
@@ -14,7 +16,9 @@ class importproperty extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            xml: "<x>XML Goes Here</x>"
+            xml: "<x>Remove this</x>",
+            isLoading: false,
+            jsonResult: { result: "result" }
         }
 
         this.handleInputChange = this.handleInputChange.bind(this);
@@ -31,42 +35,63 @@ class importproperty extends Component {
     handleSubmit(e) {
         e.preventDefault()
         console.log("Running");
-        axios.post(`http://${process.env.REACT_APP_NodeURL}?status=draft`,`${this.state.xml}`,{
-            headers:{
-                "Content-Type":"application/xml"
+        this.setState({
+            isLoading: true
+        })
+        axios.post(`http://${process.env.REACT_APP_NodeURL}?status=draft`, `${this.state.xml}`, {
+            headers: {
+                "Content-Type": "application/xml"
             }
-        }).then(res=>{
+        }).then(res => {
             console.log(res);
             Swal.fire({
                 icon: 'success',
                 title: 'Uploaded',
                 text: res
             })
-        }).catch(err=>{
+            this.setState({
+                jsonResult: res.data
+            })
+        }).catch(err => {
             console.error(err);
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: err
             })
+
+        }).finally(e => {
+            this.setState({
+                isLoading: false
+            })
         })
+
     }
 
     render() {
         return (
             <div className='container'>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <textarea value={this.state.xml} onChange={this.handleInputChange} name="xml" className='xml-area' rows="20" />
-                    </div>
-                    <div>
-                        <input type="submit" value="Submit" />
-                    </div>
-                </form>
-                <div className='xml-viewer'>
-                    <XMLViewer xml={`"${this.state.xml}"`} />
-                </div>
-
+                {
+                    this.state.isLoading ?
+                        <div className='loadblock'><div className="lds-facebook" ><div></div><div></div><div></div></div></div>
+                        :
+                        <div>
+                            <form onSubmit={this.handleSubmit}>
+                                <div>
+                                    <textarea value={this.state.xml} onChange={this.handleInputChange} name="xml" className='xml-area' rows="20" />
+                                </div>
+                                <div>
+                                    <input type="submit" value="Submit" />
+                                </div>
+                            </form>
+                            <div className='xml-viewer'>
+                                <XMLViewer xml={`"${this.state.xml}"`} />
+                            </div>
+                            <div className='xml-viewer'>
+                                <ReactJson src={this.state.jsonResult} />
+                            </div>
+                        </div>
+                }
             </div>
         );
     }
