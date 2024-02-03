40c34dd1baeacb7041459d873112534a48b9a409
Pawan Piumal <pawanpiumal1@gmail.com>
2024-02-03 18:07:04 +0530

    4

--- client/src/Components/Header/header.jsx
+++ client/src/Components/Header/header.jsx
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './header.css';
 
 import { withRouter } from '../withRouter';
+import { Link } from 'react-router-dom';
 class header extends Component {
     render() {
         return (
             <div>
                 <nav id="header-nav-bar" className="navbar " style={{ backgroundImage: `url('/Header.jpg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover' }}>
-                    <h1 className="navbar-title" onClick={() => this.props.history('/')} style={{ cursor: 'pointer' }}>Syntara Solutions</h1>
+                    <Link href="/" style={{textDecoration:'none', width:'100%'}}>
+                        <h1 className="navbar-title" onClick={() => this.props.history('/')} style={{ cursor: 'pointer' }}>Syntara Solutions</h1>
+                    </Link>
                 </nav>
             </div>
         );
