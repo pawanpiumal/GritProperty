8e056956f1aee9721cad7514de9149605738ce1f
Pawan Piumal <pawanpiumal1@gmail.com>
2024-02-05 18:58:10 +0530

    Cards

--- client/src/Components/Front/Front.jsx
+++ client/src/Components/Front/Front.jsx
@@ -4,6 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 import { withRouter } from '../withRouter';
 import { jwtDecode } from 'jwt-decode';
@@ -29,74 +30,88 @@ class Front extends Component {
             <Container>
                 <Row style={{ margin: '5%' }}>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('Login.jpg')` }} className="mb-2" onClick={e => this.reRoute('/login')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Login </Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/login">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('Login.jpg')` }} className="mb-2" >
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Login </Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('Import.jpg')`, boxShadow: 'inset 0 0 100px #000' }} className="mb-2" onClick={e => this.reRoute('/import')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Import </Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/import">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('Import.jpg')`, boxShadow: 'inset 0 0 100px #000' }} className="mb-2">
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Import </Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('Error.webp')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" onClick={e => this.reRoute('/errors')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Errors SQL</Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/errors">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('Error.webp')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" >
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Errors SQL</Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('ErrorFile.webp')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" onClick={e => this.reRoute('/errorFiles')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Errors Files </Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/errorFiles">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('ErrorFile.webp')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2"  >
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Errors Files </Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('configImage.jpg')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" onClick={e => this.reRoute('/Config')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Config </Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/config">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('configImage.jpg')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2">
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Config </Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('imports.jpg')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" onClick={e => this.reRoute('/imports')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Imports </Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/imports">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('imports.jpg')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" F>
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Imports </Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                     <Col>
-                        <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('uploads.jpg')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2" onClick={e => this.reRoute('/uploads')} >
-                            {/* <Card.Header></Card.Header> */}
-                            <Card.Body >
-                                <Card.Title> Uploads </Card.Title>
-                                <Card.Text style={{ height: '8rem' }}>
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        <Link to="/uploads">
+                            <Card bg='dark' key='Dark' text='white' style={{ width: '22rem', cursor: 'pointer', backgroundImage: `url('uploads.jpg')`, backgroundSize: 'cover', boxShadow: 'inset 0 0 100px #000' }} className="mb-2">
+                                {/* <Card.Header></Card.Header> */}
+                                <Card.Body >
+                                    <Card.Title> Uploads </Card.Title>
+                                    <Card.Text style={{ height: '8rem' }}>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Link>
                     </Col>
                 </Row>
             </Container>
